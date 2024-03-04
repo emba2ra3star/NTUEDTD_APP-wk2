2af5904742297e9044d8fb0a51fb7e755cf3d24e
emba2ra3star <tachi.i.061@gmail.com>
2024-03-04 21:30:06 +0800

    retry

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, StatusBar, SafeAreaView } from "react-native";
+import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList";
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <Header />
+      <AlbumList />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:"#fff"
+  },
+});
+
+export default App;
